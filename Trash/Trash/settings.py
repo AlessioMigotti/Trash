5dddfd6d40cd3b86f34cff2fe89e41b031fb6324
AlessioMigotti <migottialessiodev@gmail.com>
2023-03-21 12:36:30 +0000

    Add to project

--- Trash/Trash/settings.py
+++ Trash/Trash/settings.py
@@ -9,8 +9,8 @@ https://docs.djangoproject.com/en/3.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.2/ref/settings/
 """
-
-from pathlib import Path
+from pathlib import Path 
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -121,6 +121,8 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
+MEDIA_ROOT = os.path.join(BASE_DIR, 'MEDIA')
+MEDIA_URL = '/MEDIA/'
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
--- Trash/client/views.py
+++ Trash/client/views.py
@@ -9,3 +9,12 @@ class Index(View):
 class About(View):
     def get(self, request, *args, **kwargs):
         return render(request,'client/about.html')
+
+class Order(View):
+    def get(self, request, *args, **kwargs):
+        pass
+        # get every item from each category
+
+        #pass into context
+
+        #render the template
